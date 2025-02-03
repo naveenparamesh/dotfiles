603cb3aaf3215c4554975b699d17af25ef3069d1
Naveen Paramesh <naveenparamesh@gmail.com>
2025-02-02 22:03:08 -0500

    Added hop.nvim for easymotion like vim motion

---
+++ .config/nvim/lua/naveen/plugins/hop.lua
@@ -0,0 +1,12 @@
+return {
+  "smoka7/hop.nvim",
+  version = "*",
+  opts = {
+    keys = "etovxqpdygfblzhckisuran",
+  },
+  keys = {
+    { "<leader>ha", "<cmd>HopAnywhere<cr>", desc = "Hop Anywhere" },
+    { "<leader>hw", "<cmd>HopWord<cr>", desc = "Hop Word" },
+    { "<leader>hpt", "<cmd>HopPattern<cr>", desc = "Hop Pattern" },
+  },
+}
